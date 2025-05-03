2e040a575d1454986a9871f27f8c01dc3082eca9
maxim-francesco <maaximfrancesco@gmail.com>
2025-05-04 01:40:55 +0300

    created sign in first comp without styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignInComponent } from './features/auth/sign-in/sign-in.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SignInComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'journal-app';
