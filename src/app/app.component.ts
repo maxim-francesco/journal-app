d57cc87d90c928f19ec9669ea586c34107ba261e
maxim-francesco <maaximfrancesco@gmail.com>
2025-05-04 03:48:50 +0300

    created fill bio comp and styled it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignInComponent } from './features/auth/sign-in/sign-in.component';
+import { ProfileComponent } from './features/auth/profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SignInComponent],
+  imports: [RouterOutlet, SignInComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
