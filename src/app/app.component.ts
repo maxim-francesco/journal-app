8dd7a5a83157cc44602589fd684ae08fda79c4f3
maxim-francesco <maaximfrancesco@gmail.com>
2025-05-04 03:57:50 +0300

    added thanks comp for bio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignInComponent } from './features/auth/sign-in/sign-in.component';
 import { ProfileComponent } from './features/auth/profile/profile.component';
+import { ThanksComponent } from './features/auth/thanks/thanks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SignInComponent, ProfileComponent],
+  imports: [RouterOutlet, SignInComponent, ProfileComponent, ThanksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
