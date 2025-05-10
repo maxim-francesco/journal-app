8b840821ea540285fe67c850a2256c8fbbc64e9b
maxim-francesco <maaximfrancesco@gmail.com>
2025-05-10 19:14:03 +0300

    added remove and export button

--- src/app/features/jurnal/card/card.component.ts
+++ src/app/features/jurnal/card/card.component.ts
@@ -1,12 +1,31 @@
-import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
 export class CardComponent {
   title = input.required<string>();
   date = input.required<string>();
+
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (
+      this.isMenuOpen &&
+      !target.closest('.text-3xl') &&
+      !target.closest('.absolute')
+    ) {
+      this.isMenuOpen = false;
+    }
+  }
 }
